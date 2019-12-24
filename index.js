f7b79a9d9982073a808a1b7ac0dd56286bc932e6
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-24 16:10:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a,b){
+  return(a+b)
+}
+function subtract(a,b){
+  return(b-a)
+}
+function multiply(a,b){
+  return(b*a)
+}
+function divide(a,b){
+  return(a/b)
+}
+function dec(n){
+  return(n--)
+}
